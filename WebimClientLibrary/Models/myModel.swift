133c38f87d78c63ede1425687f90e0aac370ce4e
Blyabtroi <blyabtroi@gmail.com>
2019-05-17 15:22:06 +0300

    changes in master

---
+++ WebimClientLibrary/Models/myModel.swift
@@ -0,0 +1,9 @@
+//
+//  myModel.swift
+//  WebimClientLibrary_Example
+//
+//  Created by Vasiliy Kozlov on 17/05/2019.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import Foundation
---
+++ myWorkspace2/myWorkspace2/myWorkspace2.h
@@ -0,0 +1,19 @@
+//
+//  myWorkspace2.h
+//  myWorkspace2
+//
+//  Created by Vasiliy Kozlov on 17/05/2019.
+//  Copyright © 2019 Me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for myWorkspace2.
+FOUNDATION_EXPORT double myWorkspace2VersionNumber;
+
+//! Project version string for myWorkspace2.
+FOUNDATION_EXPORT const unsigned char myWorkspace2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <myWorkspace2/PublicHeader.h>
+
+
