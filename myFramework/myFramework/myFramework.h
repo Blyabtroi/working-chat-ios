b62908329dd689ba1b466eb98c2287ae9dc82056
Blyabtroi <blyabtroi@gmail.com>
2019-05-17 15:19:26 +0300

    no message

---
+++ myFramework/myFramework/myFramework.h
@@ -0,0 +1,19 @@
+//
+//  myFramework.h
+//  myFramework
+//
+//  Created by Vasiliy Kozlov on 17/05/2019.
+//  Copyright © 2019 Me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for myFramework.
+FOUNDATION_EXPORT double myFrameworkVersionNumber;
+
+//! Project version string for myFramework.
+FOUNDATION_EXPORT const unsigned char myFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <myFramework/PublicHeader.h>
+
+
